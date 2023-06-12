ca6128b3999b6ac2f902fd1fcf1cab04033645c8
Luiz Lima <lima.luizgo@gmail.com>
2023-06-12 15:36:59 -0300

    HomePage implementation

--- frontend/src/app/components/Header/index.tsx
+++ frontend/src/app/components/Header/index.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 export default function Header() {
   return (
-    <header>
+    <header className='header-component'>
       <div>
         <a href='/'>
           <Image
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,32 @@
 import Header from '@/app/components/Header';
 import React from 'react';
-import './page.module.scss';
+import Image from 'next/image';
+import styles from './page.module.scss';
 
 export default function Home() {
   return (
     <>
       <Header />
+      <main className={styles.main}>
+        <div>
+          <h1>Soluções em gestão de tarefas.</h1>
+          <p>
+            Lorem Ipsum é simplesmente uma simulação de texto da indústria
+            tipográfica e de impressos, e vem sendo utilizado desde o século
+            XVI, quando um impressor desconhecido pegou uma bandeja de tipos e
+            os embaralhou para fazer um livro de modelos de tipos.
+          </p>
+          <button>Saiba mais</button>
+        </div>
+        <div>
+          <Image
+            src='/homeimg.png'
+            alt='Ilustração da página inicial'
+            height={710}
+            width={710}
+          />
+        </div>
+      </main>
     </>
   );
 }
